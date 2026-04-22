53fbf8f5d15c96903caed854c305445f4154ae94
YashD76 <a250048@famt.ac.in>
2026-04-22 16:02:44 +0530

    Commited for practical 4

---
+++ node.js
@@ -0,0 +1,17 @@
+// Import HTTP module
+const http = require('http');
+
+// Define port number
+const PORT = 3000;
+
+// Create server
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World! Node.js application deployed using Jenkins Pipeline.');
+    res.end();
+});
+
+// Start server
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
